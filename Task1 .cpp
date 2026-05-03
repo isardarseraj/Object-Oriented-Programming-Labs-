4f1f5a014644f639bf454973687e261a57eb58f6
SERAJ KHAN MUGHAL <sardarseraj1122@gmail.com>
2026-05-03 10:14:32 +0500

    Create Task1 .cpp

---
+++ Task1 .cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct Student
+{
+    string first_Name;
+    string last_Name;
+    int roll_Number;
+    float marks;
+
+    void displayStudentInfo()
+    {
+        cout << "Full Name: " << first_Name << " " << last_Name << endl;
+        cout << "Roll Number: " << roll_Number << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s1;
+        // here we assign values in thee code.
+    s1.first_Name = "Sardar";
+    s1.last_Name = "Seraj";
+    s1.roll_Number = 101;
+    s1.marks = 90;
+
+    // here we call member funtion.
+    
+    s1.displayStudentInfo();
+    return 0;
+}
